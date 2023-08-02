552faf6d26dd1182a4df8c7568cae6ffc14338aa
Maithanh0101 <139963432+Maithanh0101@users.noreply.github.com>
2023-08-02 21:17:38 +0700

    add feed apis

--- LearningHub/src/Pages/Newsfeeds.jsx
+++ LearningHub/src/Pages/Newsfeeds.jsx
@@ -3,12 +3,132 @@ import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, Context
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../dashboardComponents';
 import "../dashBoard/dashboard.css"
+
+import { useState, useEffect } from 'react';
+
 const Newsfeed = () => {
-  const editing = { allowDeleting: true, allowEditing: true };
+
+  const [data, setData] = useState([]);
+  const [selectedFeed, setSelectedFeed] = useState({});
+  const [editing, setEditing] = useState(false);
+  useEffect(() => {    
+    fetch('http://127.0.0.1:5000/api/get_posts')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleDelete = (id) => {
+    // Make a DELETE request to the API to delete the feed with the given id
+    fetch(`http://127.0.0.1:5000/api/post_delete/${id}`, {
+      method: 'DELETE',
+    })
+    .then(() => {
+      // Update the state to remove the deleted feed
+      setData(data.filter(item => item.id !== id));
+    })
+    .catch(error => console.error('Error deleting data:', error));
+  };
+
+  const handleEdit = (feed) => {
+    setSelectedFeed(feed);
+    setEditing(true);
+  };
+
+  const handleAdd = () => {
+    setSelectedFeed({});
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    // Make a POST or PUT request to the API to add or update the feed
+    const method = selectedFeed.id ? 'PUT' : 'POST';
+    const url = selectedFeed.id
+      ? `http://127.0.0.1:5000/api/post_update/${selectedFeed.id}`
+      : 'http://127.0.0.1:5000/api/add_post';
+      
+    fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(selectedFeed),
+    })
+    .then(response => response.json())
+    .then(newFeed => {
+      if (selectedFeed.id) {
+        // Update existing feed
+        setData(data.map(item => (item.id === newFeed.id ? newFeed : item)));
+      } else {
+        // Add new feed
+        setData([...data, newFeed]);
+      }
+      setEditing(false);
+      setSelectedFeed({});
+    })
+    .catch(error => console.error('Error saving data:', error));
+  };
+
+  
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Newsfeed" />
-    </div>
+      <Header category="Page" title="News Feed" />
+   
+      <h1>POSTs</h1>
+      <button class='btn btn-primary' onClick={handleAdd}>Add New Feed</button>
+
+      {editing && (
+          <div>
+           
+          <h1>{selectedFeed.id ? 'Edit Feed' : 'Add New Feed'}</h1>
+          <span>Title:</span>
+          <input
+            type="text"
+            placeholder="Title"
+            value={selectedFeed.title || ''}
+            onChange={(e) => setSelectedFeed({ ...selectedFeed, title: e.target.value })}
+          />
+          <h2></h2>
+          <span>Content:</span>
+          <input
+            type="text"
+            placeholder="Content"
+            value={selectedFeed.content || ''}
+            onChange={(e) => setSelectedFeed({ ...selectedFeed, content: e.target.value })}
+          />
+           <h2></h2>
+          <button class="btn btn-primary" onClick={handleSave}>Save</button>
+          <h2></h2>
+        </div>
+
+      )}
+
+        <table class='table' >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Content</th>
+              <th >Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(item => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.content}</td>
+                <td>
+                <button class='btn btn-primary' onClick={() => handleEdit(item)}>Edit</button>
+                <button class='btn btn-danger' onClick={() => handleDelete(item.id)}>Delete</button>
+
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
   );
 };
 export default Newsfeed;
