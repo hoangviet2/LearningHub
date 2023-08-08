78d2f6661b922c1ab2299c6a7f5d3b1069726d7c
Maithanh0101 <139963432+Maithanh0101@users.noreply.github.com>
2023-08-09 00:51:30 +0700

    uppp

--- LearningHub/src/Pages/Newsfeeds.jsx
+++ LearningHub/src/Pages/Newsfeeds.jsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
-import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import React, { useState, useEffect } from 'react';
 import { Header } from '../dashboardComponents';
 import "../dashBoard/dashboard.css"
 
-import { useState, useEffect } from 'react';
-
 const Newsfeed = () => {
-  const [editingg,setEditingg] =  useState(false);
-
   const [data, setData] = useState([]);
   const [selectedFeed, setSelectedFeed] = useState({});
   const [editing, setEditing] = useState(false);
-  useEffect(() => {    
-    fetch('https://feeds.xiganglive.vip/addpost')
+
+  useEffect(() => {
+    fetch('https://feeds.xiganglive.vip/api/get_posts')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error));
@@ -21,7 +16,7 @@ const Newsfeed = () => {
 
   const handleDelete = (id) => {
     // Make a DELETE request to the API to delete the feed with the given id
-    fetch(`https://feeds.xiganglive.vip/deletepost/${id}`, {
+    fetch(`https://feeds.xiganglive.vip/api/post_delete/${id}`, {
       method: 'DELETE',
     })
     .then(() => {
@@ -38,16 +33,15 @@ const Newsfeed = () => {
 
   const handleAdd = () => {
     setSelectedFeed({});
-    setEditingg(true);
+    setEditing(true);
   };
 
   const handleSave = () => {
-    setEditingg(false);
     // Make a POST or PUT request to the API to add or update the feed
     const method = selectedFeed.id ? 'PUT' : 'POST';
     const url = selectedFeed.id
-      ? `https://feeds.xiganglive.vip/editpost${selectedFeed.id}`
-      : 'https://feeds.xiganglive.vip/postadd';
+      ? `https://feeds.xiganglive.vip/api/post_update/${selectedFeed.id}`
+      : 'https://feeds.xiganglive.vip/api/add_post';
       
     fetch(url, {
       method,
@@ -71,62 +65,54 @@ const Newsfeed = () => {
     .catch(error => console.error('Error saving data:', error));
   };
 
-  
   return (
     <div classnameName="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="News Feed" />
-   
+    <Header category="Page" title="News Feed" />
       <h1>POSTS</h1>
-      <button classname='btn btn-primary' onClick={handleAdd}>Add New Feed</button>
-
-      {editingg && (
-          <div>
-          <h1>{selectedFeed.id ? 'Edit Feed' : 'Add New Feed'}</h1>
-          <span>Title:</span>
+      <button type="button" class="btn btn-outline-danger" onClick={handleAdd}>Add New Feed</button>
+      {editing && (
+        <div>
+          <h2>{selectedFeed.id ? 'Edit Feed' : 'Add New Feed'}</h2>
           <input
             type="text"
             placeholder="Title"
             value={selectedFeed.title || ''}
             onChange={(e) => setSelectedFeed({ ...selectedFeed, title: e.target.value })}
           />
-          <h2></h2>
-          <span>Content:</span>
           <input
             type="text"
             placeholder="Content"
             value={selectedFeed.content || ''}
             onChange={(e) => setSelectedFeed({ ...selectedFeed, content: e.target.value })}
           />
-           <h2></h2>
-          <button classname="btn btn-primary" onClick={handleSave}>Save</button>
-          <h2></h2>
+          <button  type="button" class="btn btn-outline-success" onClick={handleSave}>Save</button>
         </div>
-
       )}
-
-        <table classname='table' >
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Content</th>
-              <th >Action</th>
-            </tr>
-            </thead>
-        <tbody>
+      <div class="container px-4">
+          <table classname='table' >
+        <thead>
+          <tr>
+            <th row mt-8 scope="col">ID</th>
+            <th row mt-8 scope="col">Title</th>
+            <th row mt-8 scope="col">Content</th>
+            <th row mt-8 scope="col">Action</th>
+          </tr>
+          </thead>
+      <tbody>
           {data.map(item => (
-            <tr key={item.id}>
+            <tr scope="col" key={item.id}>
               <td>{item.id}</td>
               <td>{item.title}</td>
               <td>{item.content}</td>
               <td>
-                <button onClick={() => handleEdit(item)}>Edit</button>
-                <button onClick={() => handleDelete(item.id)}>Delete</button>
+                <button type="button" class="btn btn-outline-primary" onClick={() => handleEdit(item)}>Edit</button>
+                <button type="button" class="btn btn-outline-danger" onClick={() => handleDelete(item.id)}>Delete</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+    </div>
     </div>
   );
 };
