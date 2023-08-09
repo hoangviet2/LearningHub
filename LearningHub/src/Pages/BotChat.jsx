f1b24aa931edfe4f1a8ddf9cd28107276177f474
hoangviet2 <hoangviet18306@gmail.com>
2023-08-09 14:33:25 +0700

    merge commit

--- LearningHub/src/Pages/BotChat.jsx
+++ LearningHub/src/Pages/BotChat.jsx
@@ -17,4 +17,6 @@ const botChat = () => {
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error));
-  }, []) ; }
+  }, []) ;
+}
+export default botChat
--- LearningHub/src/Pages/index.jsx
+++ LearningHub/src/Pages/index.jsx
@@ -1,5 +1,5 @@
 export { default as Ecommerce } from './Ecommerce';
-export { default as Kanban } from './Kanban';
+export { default as BotChat } from './BotChat';
 export { default as Employees } from './Employees';
 export { default as Editor } from './Editor';
 export { default as Customers } from './Customers';
